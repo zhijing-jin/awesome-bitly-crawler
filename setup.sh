8e76ecf172e11ee8a9ef7ac3babd4ad739fdfe40
Ubuntu <ubuntu@ip-172-31-15-100.us-east-2.compute.internal>
2019-10-07 07:54:18 +0000

    deleted unnecessary files

--- setup.sh
+++ setup.sh
@@ -5,6 +5,5 @@ pip3 install efficiency requests tqdm lxml
 tmux
 mkdir temp
 cd temp
-curl -O https://raw.githubusercontent.com/zhijing-jin/crawler/master/bitly_get_clicks.py
 python3 bitly_get_clicks.py -len 3 -shard 8
 python3 bitly_get_clicks.py -len 4 -use_proxy -proxy_file proxies2.txt -shard 2
