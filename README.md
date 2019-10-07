214bbddd6a9b56dc97fd80be3c8c83b36ecb751c
Ubuntu <ubuntu@ip-172-31-15-100.us-east-2.compute.internal>
2019-10-07 13:19:26 +0000

    added rotating user_agents

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
-# crawler
+# Crawler
+This is a Python web crawler to catch bit.ly clickrates.
 
-
-
+## How to Run
+```bash
+python bitly_get_clicks.py -shard 0
+```
 
 ### File source
 
-The [data](data/) folder is created by
+Note that the uid files in the [data](data/) folder is created by
 ```bash
 python make_permutations.py
 ```
--- bitly_get_clicks.py
+++ bitly_get_clicks.py
@@ -118,28 +118,32 @@ def get_html(url, use_proxy=False):
     from efficiency.log import show_time
 
     time.sleep(interval)
+    headers = {'User-Agent': next(user_agents)}
 
     if use_proxy:
         proxy = next(proxy_pool)
 
         try:
-            r = requests.get(url, proxies={"http": proxy, "https": proxy})
+            r = requests.get(url, proxies={"http": proxy, "https": proxy},
+                             headers=headers)
         except Exception:
             proxy_pool.add_bad_proxy(proxy)
             get_html(url)
             return
     else:
         try:
-            r = requests.get(url)
+            r = requests.get(url, headers=headers)
         except:
-            show_time('[Warn] {} is blocked'.format(url))
+            show_time(
+                '[Warn] {} is blocked for {}s'.format(url, sleeper.block_secs))
             save_json()
 
             sleeper.sleep(url)
-            r = requests.get(url)
+            r = requests.get(url, headers=headers)
 
     if r.status_code == 403:
-        show_time('[Warn] {} is blocked'.format(url))
+        show_time(
+            '[Warn] {} is blocked for {}s'.format(url, sleeper.block_secs))
         save_json()
 
         sleeper.sleep(url)
@@ -234,6 +238,7 @@ def main(shard_id, length=7, save_size=2000, shard_size=20000):
 
 if __name__ == '__main__':
     import argparse
+    from itertools import cycle
 
     parser = argparse.ArgumentParser('Args for Web Crawler')
     parser.add_argument('-len', default=7, type=int,
@@ -269,5 +274,14 @@ if __name__ == '__main__':
     interval = int(60 * 60 / args.hour_max)
     save_to = 'ctr_{:03d}.json'.format(args.shard)
     data = get_init_data()
+    user_agents = cycle([
+        "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
+        "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+        "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
+        "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+        "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+        "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
+    ])
 
     main(args.shard, length=args.len, shard_size=args.shard_size)
