72ee934c118f38e3374dbb4b218db99e660d93a0
Ubuntu <ubuntu@ip-172-31-15-100.us-east-2.compute.internal>
2019-10-07 07:55:34 +0000

    del files

--- setup.sh
+++
@@ -1,9 +0,0 @@
-sudo apt update
-sudo apt install python3-pip
-
-pip3 install efficiency requests tqdm lxml
-tmux
-mkdir temp
-cd temp
-python3 bitly_get_clicks.py -len 3 -shard 8
-python3 bitly_get_clicks.py -len 4 -use_proxy -proxy_file proxies2.txt -shard 2
